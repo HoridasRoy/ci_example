541282ce6679a3b47b3fe8dd726f12bf9270b93a
Horidas Roy <horidasr@gmail.com>
2024-08-30 00:10:39 +0600

    Logger added.

--- src/main/java/com/example/ci_example/CiExampleApplication.java
+++ src/main/java/com/example/ci_example/CiExampleApplication.java
@@ -1,13 +1,21 @@
 package com.example.ci_example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 
 @SpringBootApplication
 public class CiExampleApplication {
 
+	public static final Logger logger = LoggerFactory.getLogger(CiExampleApplication.class);
 	public static void main(String[] args) {
+		logger.info("Spring boot application is running.");
 		SpringApplication.run(CiExampleApplication.class, args);
+
+
 	}
 
 }
